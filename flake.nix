27849a23b7dff02aa226faaffb3b0f6466d82f90
N4tonh <n4tonh@proton.me>
2026-03-29 16:05:40 -0400

    29 de marzo temprano

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Mi configuración de NixOS con Flakes y Home Manager";
 
   inputs = {
+  	determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
   	nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
         home-manager = {
         	url = "github:nix-community/home-manager";
@@ -9,13 +10,14 @@
 	};
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, determinate, ... }@inputs: {
   	nixosConfigurations.twarlien = nixpkgs.lib.nixosSystem
 {
 	system = "x86_64-linux";
 	specialArgs = { inherit inputs; };
 	modules = [
 		./configuration.nix
+		determinate.nixosModules.default
 		home-manager.nixosModules.home-manager
 		{
 			home-manager.useGlobalPkgs = true;
